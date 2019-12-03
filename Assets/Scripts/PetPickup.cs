6d8fb77d0b1c864fdbf2fe8c02f0d5055614ad5a
Ricardo Escobar <rescobar713@gmail.com>
2019-12-03 18:45:44 -0500

    added pet pickup script

--- Assets/Scripts/PetPickup.cs
+++ Assets/Scripts/PetPickup.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class PetPickup : MonoBehaviour
 {
+    public Vector3 heldPosition;
+
+    private bool isHeld;
+
+    private int numBumps;
+    public int bumpsToDrop = 6;
+
+    private Collider col;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        col = GetComponent<Collider>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (!isHeld)
+            {
+                isHeld = true;
+                transform.parent = other.transform.GetChild(0);
+                transform.localRotation = Quaternion.identity;
+                transform.localPosition = heldPosition;
+                col.enabled = false;
+            }
+        }
     }
 }
